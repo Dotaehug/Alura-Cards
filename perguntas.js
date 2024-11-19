7c6b33f11cc1c39eff6690f07f3db2b9c397c59e
Maria <00001138915270sp@al.educacao.sp.gov.br>
2024-11-19 12:08:52 -0300

    Primeira versão do meu código

---
+++ perguntas.js
@@ -0,0 +1,71 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript',
+    'O JavaScript é uma linguagem de programação.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI).'
+)
+
+criaCartao(
+    'Filosofia',
+    'De quem é a famosa frase “Penso, logo existo”?',
+    'Originada do françês, essa frase pertence ao filósofo René Descartes.'
+)
+
+criaCartao(
+    'Física',
+    'Qual a velocidade da luz?',
+    '299 792 458 metros por segundo (m/s).'
+)
+
+criaCartao(
+    'História',
+    'Quem foi a primeira pessoa a viajar no Espaço?',
+    'O russo Yuri Gagarin, em 12 de abril de 1961.'
+)
+
+criaCartao(
+    'Física',
+    'Quem foi o cientista que propôs a teoria da relatividade?',
+    'Foi Albert Einstein.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a língua com mais falantes nativos no mundo?',
+    'O mandarim, com 1,1 bilhão de falantes nativos.'
+)
+
+criaCartao(
+    'Artes e Cultura',
+    'Qual artista é conhecido como o Rei do Pop?',
+    'O cantor norte-americano Michael Jackson.'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual parte da planta é responsável pela fotossíntese?',
+    'A parte da planta responsãvel pela fotossíntese é a folha.'
+)
+
+criaCartao(
+    'Artes e Cultura',
+    'De qual movimento artístico fazia parte o pintor Salvador Dalí?',
+    'Do Movimento Surrealista.'
+)
